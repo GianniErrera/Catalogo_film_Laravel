d5670efc879b5c7a249d438423f900518bce1b80
GianniErrera <giannierrera@yahoo.it>
2018-12-18 19:36:42 +0100

    Modificata vista lista film, precedentemente quando si usava la tendina di filtro una linea di codice dava un errore, adesso dovrebbe funzionare

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -31,24 +31,29 @@
 <table>
 @foreach ($films as $film)
 <tr>
-  
-{{--   @if ($film->locandina)  --}}
-@if ($film->locandina)
-  @if (file_exists("storage/locandine/thumbnails/" . $film->id . "/" . $film->locandina->immagine))  
+
+ @if (isset($film->locandina))  
+
+    @if (file_exists("storage/locandine/thumbnails/" . $film->id . "/" . $film->locandina->immagine))  
 
 
-   <td style = "margin:5px"; width="10%"><img src = "/storage/locandine/thumbnails/{{$film->id}}/{{$film->locandina->immagine}}" width = "80%"; vertical-align: top; ></td>
+    <td style = "margin:5px"; width="10%"><img src = "/storage/locandine/thumbnails/{{$film->id}}/{{$film->locandina->immagine}}" width = "80%"; vertical-align: top; ></td>
     
-  @else 
-{{--   <td>"/storage/locandine/thumbnails/{{$film->id}}/{{$film->locandina->immagine}}"</td> --}}
- <td style = "margin:5px"; width="10%"><img src = "https://via.placeholder.com/300x480.png/" width = "80%"; vertical-align: top; ></td>
-  {{-- <td style = "margin:5px"; width="10%"><img src = "https://via.placeholder.com/300x480.png/" width = "80%"; vertical-align: top; ></td> --}}
-  @endif	
+    @else 
+{{--     <td>"/storage/locandine/thumbnails/{{$film->id}}/{{$film->locandina->immagine}}"</td> --}}
+   <td style = "margin:5px"; width="10%"><img src = "https://via.placeholder.com/300x480.png/" width = "80%"; vertical-align: top; ></td>
+{{--     <td style = "margin:5px"; width="10%"><img src = "https://via.placeholder.com/300x480.png/" width = "80%"; vertical-align: top; ></td> --}}
+     @endif	
 
 @else 
 <td style = "margin:5px"; width="10%"><img src = "https://via.placeholder.com/300x480.png/" width = "80%"; vertical-align: top; ></td>
 @endif
 
+
+
+{{--  @if ($film->locandina) <td style = "margin:5px" width="10%"><img src = "/storage/locandine/{{$film->id}}/{{$film->locandina->immagine}}/" width = "80%"></td> --}}
+{{-- @else <td></td>
+@endif  --}}  
 
 <td>
 <a href = "/films/{{$film->id}}" ><h3>{{$film->titolo}}</a> - {{$film->anno}}
